3cc6de8f04d73f38697016d53d7d1de23212a8da
piiyaaq <yadeepranpreeya@gmail.com>
2025-06-17 11:24:41 +0700

    merge

---
+++ cell03/advaanced.py
@@ -0,0 +1,8 @@
+def print_multiplication_tables():
+    print("Multiplication Tables")
+    print("---------------------")
+
+    for i in range(1,11):
+      print(f"Table for {i}:")
+      for j in range(1,110=):
+          result = i * j :
